07591be402e6ce62af22452e46ce665decb29ce1
Guilherme Perinotti <guilherme9115@hotmail.com>
2022-06-29 00:37:58 -0300

    resovido a formula da conjectura

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
-let numbers = [];
+let numbers = []; //array para os resultados
+let input = Number(prompt("Digite um numero para gerar a Conjectura"));
+
+numbers.push(input);
 
 function collatz(num){
+    while(num != 1){
 
-    if(num % 2 == 0){
-        num /= 2;
-        numbers.push(num);
-    } else if(num % 2 != 0){
-        num *= 3 + 1;
-        numbers.push(num);
+        //se o numero em questao for par divide ele por 2
+        if(num % 2 == 0){
+            num = num / 2;
+            numbers.push(num) //adiciona o resultado ao array
+            //se o numero em questao for impar multiplica ele por 3 e soma 1
+        } else if(num % 2 != 0){
+            num = num * 3 + 1;
+            numbers.push(num) //adiciona o resultado ao array
+        }
     }
-    return collatz(num);
 }
-collatz(4)
 
-document.write(numbers)
-
+collatz(input)
 //numbers.join(" -> ")
-
-//console.log(collatz(10))
+console.log(numbers.join(" -> "));
